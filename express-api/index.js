78922511d89d74abea0e2cc53827ecf4f71dd715
Shubh152 <shubhj013@gmail.com>
2024-10-26 19:18:39 +0530

    change listen

--- express-api/index.js
+++ express-api/index.js
@@ -25,10 +25,11 @@ app.use(cors());
 dotenv.config();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(PORT, () => {
-  console.log(`server is listening at port:${PORT}`);
-});
-
+if (require.main == module) {
+  app.listen(PORT, () => {
+    console.log(`server is listening at port:${PORT}`);
+  });
+}
 app.post("/signin", async (req, res) => {
   try {
     const userName = req.body.username;
